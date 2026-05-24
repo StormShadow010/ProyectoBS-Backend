efd13358c7723092d7d5f0b260cec7403e9e2cec
Santiago <asjimenezg1923@gmail.com>
2026-05-24 13:07:13 -0500

    Final  Module

--- src/modules/usuarios/usuarios.controller.ts
+++ src/modules/usuarios/usuarios.controller.ts
@@ -4,8 +4,11 @@ import { createUsuarioSchema, updateUsuarioSchema } from "./usuarios.schema";
 import { ok, created, fail, serverError } from "../../utils/response";
 
 export const getAll = async (_req: Request, res: Response) => {
-  try { return ok(res, await svc.getAll()); }
-  catch (e) { return serverError(res, e); }
+  try {
+    return ok(res, await svc.getAll());
+  } catch (e) {
+    return serverError(res, e);
+  }
 };
 
 export const getById = async (req: Request, res: Response) => {
@@ -13,16 +16,19 @@ export const getById = async (req: Request, res: Response) => {
     const item = await svc.getById(Number(req.params.id));
     if (!item) return fail(res, "Usuario no encontrado", 404);
     return ok(res, item);
-  } catch (e) { return serverError(res, e); }
+  } catch (e) {
+    return serverError(res, e);
+  }
 };
 
 export const create = async (req: Request, res: Response) => {
   try {
     const data = createUsuarioSchema.parse(req.body);
-    return created(res, await svc.create(data), "Usuario creado");
+    return created(res, await svc.create(data), "Usuario creadoo");
   } catch (e: any) {
     if (e?.errors) return fail(res, e.errors[0]?.message ?? "Datos inválidos");
-    if ((e as any)?.code === "23505") return fail(res, "Username o email ya existe", 409);
+    if ((e as any)?.code === "23505")
+      return fail(res, "Username o email ya existe", 409);
     return serverError(res, e);
   }
 };
@@ -44,5 +50,7 @@ export const remove = async (req: Request, res: Response) => {
     const item = await svc.remove(Number(req.params.id));
     if (!item) return fail(res, "Usuario no encontrado", 404);
     return ok(res, item, "Usuario desactivado");
-  } catch (e) { return serverError(res, e); }
+  } catch (e) {
+    return serverError(res, e);
+  }
 };
