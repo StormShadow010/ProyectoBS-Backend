82d32cb60edc9f45f559c4e0d2759ca6aea2725b
Santiago <asjimenezg1923@gmail.com>
2026-05-23 23:10:38 -0500

    Cambios

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,37 +1,36 @@
 import { Response } from "express";
 
-export const ok = (
-  res: Response,
-  data: any = null,
-  message = "Operación exitosa",
-  status = 200,
-) => {
-  return res.status(status).json({
+export const ok = (res: Response, data: any, message = "OK") => {
+  return res.status(200).json({
     success: true,
     message,
     data,
   });
 };
 
-export const fail = (
+export const created = (
   res: Response,
-  message = "Error",
-  status = 400,
-  error: any = null,
+  data: any,
+  message = "Creado correctamente",
 ) => {
+  return res.status(201).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+export const fail = (res: Response, message: string, status = 400) => {
   return res.status(status).json({
     success: false,
     message,
-    error,
   });
 };
 
 export const serverError = (res: Response, error: any) => {
-  console.error(error);
-
   return res.status(500).json({
     success: false,
-    message: "Error interno del servidor",
-    error: process.env.NODE_ENV === "development" ? error.message : undefined,
+    message: error?.message || "Error interno",
+    error,
   });
 };
